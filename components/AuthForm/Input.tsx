a3fbd2555c1cf94bff978a43597ed997bde52cc5
abdellah711 <alaouiabdellah711@gmail.com>
2022-06-20 23:15:26 +0100

    fix: label text overlays over the email input text

--- components/AuthForm/Input.tsx
+++ components/AuthForm/Input.tsx
@@ -6,7 +6,7 @@ const Input: FC<InputHTMLAttributes<HTMLInputElement> & { error?: boolean }> = (
         <div className="relative">
             <label className='relative group'>
                 <input {...rest} className="peer rounded-sm border border-gray-400 p-2 pt-3 w-full" required placeholder=' ' />
-                <span className='absolute grid place-items-center top-0 left-0 bottom-0 -mt-1 capitalize text-gray-500 mx-2  origin-top-left transition duration-100 group-focus-within:scale-75 group-focus-within:-translate-y-2 peer-valid:scale-75 group-focus-within:mt-0 peer-valid:mt-0 peer-valid:-translate-y-2'>{placeholder}</span>
+                <span className='absolute grid place-items-center top-0 left-0 bottom-0 capitalize text-gray-500 mx-2  origin-top-left transition duration-100 scale-75 -translate-y-2 -mt-1 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0'>{placeholder}</span>
             </label>
             {error && <XIcon className="w-7 fill-red-400 absolute top-1/2 right-2 -translate-y-1/2" />}
         </div>
