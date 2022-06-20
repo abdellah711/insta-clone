6891c3ddac9921228420ce0bfbbdd8fb29f0d221
abdellah711 <alaouiabdellah711@gmail.com>
2022-06-20 18:33:26 +0100

    feat(signup): implement signup logic with backend validation

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,37 @@
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import prisma from "lib/prisma";
+import { NextApiHandler } from "next";
+import { object, string } from 'yup'
+import bcrypt from 'bcrypt'
+
+const signup: NextApiHandler<{ message?: string }> = async (req, res) => {
+    const userData = req.body
+    if(!userData) return res.status(400).json({message:"No data provided"})
+
+    const userSchema = object({
+        email: string().email("Please enter a valid email").required(),
+        password: string().min(6).required(),
+        username: string().required(),
+        fullname: string().required()
+    })
+    
+    try {
+        const data = await userSchema.validate(userData).catch(err => res.status(400).json({ message: err.message }))
+        if (!data) return
+        data.password = await bcrypt.hash(data.password, await bcrypt.genSalt())
+        await prisma.user.create({ data })
+        res.json({ message: "Account created successfully" })
+
+    } catch (err) {
+        if (err instanceof PrismaClientKnownRequestError) {
+            if (err.code === 'P2002') {
+                return res.status(403).json({ message: `${(<string[]>err.meta?.target)?.[0]} is already registered` })
+            }
+        }
+        console.log({err})
+        res.status(500).json({message:'Something went wrong!'})
+    }
+
+}
+
+export default signup
--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,23 +1,51 @@
 import AuthForm, { RedirectCard } from "components/AuthForm"
 import Input from "components/AuthForm/Input"
-import { FormEventHandler } from "react"
+import { ChangeEventHandler, FormEventHandler, useEffect, useState } from "react"
 
 
 const Signup = () => {
+    const [formData, setFormData] = useState({ email: '', password: '', fullname: '', username: '' })
+    const [error, setError] = useState<string | null>(null)
+    const [isLoading, setIsLoading] = useState(false)
 
-    const handleSubmit: FormEventHandler<HTMLFormElement> = e => {
+
+    const handleChange: ChangeEventHandler<HTMLInputElement> = e => {
+        setFormData({ ...formData, [e.currentTarget.name]: e.currentTarget.value })
+    }
+
+    const handleSubmit: FormEventHandler<HTMLFormElement> = async e => {
         e.preventDefault()
+        setError(null)
+        setIsLoading(true)
 
+        try {
+            const resp = await fetch('/api/auth/signup', {
+                method: 'POST',
+                headers: { 'content-type': 'application/json' },
+                body: JSON.stringify(formData)
+            })
+            const { message } = await resp.json()
+            if (resp.ok) {
+                console.log({ message })
+                setIsLoading(false)
+                return
+            }
+            setError(message)
+        } catch (err) {
+            setError("Something went wrong! please check your network and try again")
+        }
+        setIsLoading(false)
     }
 
     return (
         <div className="flex items-center justify-center h-screen">
             <div className="max-w-md mx-auto flex-1">
-                <AuthForm onSubmit={handleSubmit} submitTxt="Sign up">
-                    <Input type="email" name="email" placeholder='email' />
-                    <Input type="text" name="fullname" placeholder='full name' />
-                    <Input type="username" name="username" placeholder='username' />
-                    <Input type="password" name="password" placeholder='password' />
+                <AuthForm onSubmit={handleSubmit} submitTxt="Sign up" disabled={isLoading}>
+                    {error && <p className="text-red-500 border border-red-500 py-2 px-3 rounded-sm bg-red-50">{error}</p>}
+                    <Input type="email" name="email" placeholder='email' onChange={handleChange} value={formData['email']} />
+                    <Input type="text" name="fullname" placeholder='full name' onChange={handleChange} value={formData['fullname']} />
+                    <Input type="username" name="username" placeholder='username' onChange={handleChange} value={formData['username']} />
+                    <Input type="password" name="password" placeholder='password' onChange={handleChange} value={formData['password']} />
                 </AuthForm>
                 <RedirectCard login={false} />
             </div>
