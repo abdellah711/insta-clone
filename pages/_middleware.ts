f2174250416b195dcb26ce33ec815d6a103ef343
abdellah711 <alaouiabdellah711@gmail.com>
2022-06-21 19:12:04 +0100

    fix: add middleware to redirect users to/from login page

---
+++ pages/_middleware.ts
@@ -0,0 +1,24 @@
+import { getToken } from "next-auth/jwt";
+import { NextMiddleware, NextResponse } from "next/server";
+
+
+export const middleware: NextMiddleware = async (req, ev) => {
+    const token = await getToken({ req, secret: process.env.JWT_SECRET })
+    const url = req.nextUrl.clone()
+
+    if(url.pathname.startsWith('/api/auth')) return NextResponse.next()
+
+    if (url.pathname.startsWith('/auth')) {
+        if(token){
+            url.pathname = '/'
+            return NextResponse.redirect(url)
+        }
+    } else {
+        if(!token){
+            url.pathname = '/auth/login'
+            return NextResponse.redirect(url)
+        }
+    }
+
+    return NextResponse.next()
+}
