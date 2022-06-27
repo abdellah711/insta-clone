f97cf1457e2329c42a18c14330d2caf7a1f027b9
abdellah711 <alaouiabdellah711@gmail.com>
2022-06-27 22:00:41 +0100

    style(ellipsis): truncate long texts in the posts

--- components/Post/Post.tsx
+++ components/Post/Post.tsx
@@ -1,11 +1,11 @@
-import { Like, Post, Prisma, User } from "@prisma/client"
+import type { Like, Post, User } from "@prisma/client"
 import { formatRelative } from "utils/relative-time";
 import { FC, useState } from "react"
 import LikeBtn from "./LikeBtn";
 import Link from "next/link";
 
 export type PostWithUser = (Post & {
-    _count: Prisma.PostCountOutputType;
+    _count: { likes: number };
     owner: Pick<User, "fullname" | "username" | "image" | "id">;
     likes: Like[];
 })
@@ -15,12 +15,14 @@ interface Props {
 }
 
 const Post: FC<Props> = ({ post }) => {
+    const [content, setContent] = useState(post.content.split('\n')[0].split(' ').slice(0, 10).join(' '))
 
+    const handleShowMore = () => setContent(post.content)
 
     return (
         <article className="max-w-md border border-gray-300 pb-2 rounded mb-6 bg-white">
             <header className="p-2">
-                <Link href={'/profile/'+post.owner.id}>
+                <Link href={'/profile/' + post.owner.id}>
                     <a className="flex gap-3 items-center cursor-pointer">
                         <img
                             className="w-10 rounded-full border border-gray-400"
@@ -36,7 +38,9 @@ const Post: FC<Props> = ({ post }) => {
             </div>
             <p className="p-2">
                 <span className="font-semibold">{post.owner.username} </span>
-                {post.content}
+                {content}
+                {content.length !== post.content.length
+                    && (<>...<span className="text-gray-400 cursor-pointer" onClick={handleShowMore}>more</span></>)}
             </p>
             <p className="uppercase text-xs text-gray-500 p-2">{formatRelative(post.createdAt)}</p>
         </article>
